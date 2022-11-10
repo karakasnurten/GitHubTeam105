17641eef6b97b697c5ae719a110bd5ee18a9d63e
nurtenkarakas <you@example.com>
2022-11-10 12:50:10 +0300

    first commit

---
+++ src/B02.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class B02 {
+    public static void main(String[] args) {
+        List<Integer> sayilar= new ArrayList<>();
+
+        sayilar.add(2);
+        sayilar.add(3);
+        sayilar.add(5);
+
+        // sayilar.get(1)=10;  get okumaya izin var ama deger atamaya(yazmaya) izin yok
+
+        // System.out.println(sayilar.set(1)); set varsa MUTLAKA deger atamalisin // sette yazdirma yok
+    }
+
+
+
+}
